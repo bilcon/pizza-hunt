cc31dcc5305af5636dd3c939d39373acdcd54608
Bill Morris <bilcon@swbell.net>
2022-01-21 05:33:32 -0600

    add validation

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -36,7 +36,6 @@ const handlePizzaSubmit = (event) => {
   const pizzaName = $pizzaForm.querySelector("#pizza-name").value;
   const createdBy = $pizzaForm.querySelector("#created-by").value;
   const size = $pizzaForm.querySelector("#pizza-size").value;
-  // transform DOM data into a real array of objects to execute .map()
   const toppings = [
     ...$pizzaForm.querySelectorAll("[name=topping]:checked"),
   ].map((topping) => {
@@ -59,12 +58,12 @@ const handlePizzaSubmit = (event) => {
   })
     .then((response) => response.json())
     .then((postResponse) => {
-      alert("Pizza created successfully!");
       console.log(postResponse);
     })
     .catch((err) => {
       console.log(err);
       saveRecord(formData);
+      // DO INDEXED DB STUFF HERE
     });
 };
 
